37206b28c61fdc3a33de8c2774dec04009057df8
Harel06533 <harelolguin22@gmail.com>
2023-11-13 16:36:09 -0600

    added cors middleware to express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 const dotenv = require("dotenv"); // for managing enviroment variables
 dotenv.config();
 
 const PORT = process.env["PORT"] || 3000;
 const app = express();
+
+// cors accepts every http method
+app.use(
+  cors({
+    methods: ["GET", "PUT", "POST", "DELETE", "PATCH", "UPDATE"],
+  }),
+);
 
 // only recieves json on the body
 app.use(express.json());

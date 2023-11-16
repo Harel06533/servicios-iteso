9acae0d7072c8df8eb6ee81b39bc793c54a45051
Harel06533 <harelolguin22@gmail.com>
2023-11-16 04:12:55 -0600

    started with billing ui

--- public/scripts/render_controller.js
+++ public/scripts/render_controller.js
@@ -1,5 +1,52 @@
+// controlls rendering and navigation
 "use strict";
 
+// test render, this should not be used
+function testRender(id) {
+  const div = document.createElement("div");
+  div.textContent = id;
+  renderSection(div);
+}
+
+// variables
 const main = document.querySelector("main");
-main.innerHTML = "";
-main.appendChild(createUserProfile());
+const navButtons = Array.from(
+  document
+    .getElementById("sidebarAccordion")
+    .querySelectorAll(".btn-iteso-primary-100"),
+);
+const userAccess = document
+  .getElementById("user-menu")
+  .querySelector(".user-info");
+
+// to select a section
+const avaliableSections = {
+  toSchoolBill: (data) => testRender(data),
+  toRegisterInfo: (data) => testRender(data),
+  toAcademicHistory: (data) => testRender(data),
+};
+// function to render section
+function renderSection(section) {
+  main.innerHTML = "";
+  main.appendChild(section);
+}
+
+// when user access is clicked, renders userProfile
+userAccess.addEventListener("click", (e) => {
+  e.preventDefault();
+  renderSection(createUserProfile());
+});
+
+// for moving in active sections
+navButtons.forEach((b) => {
+  b.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = b.id;
+    if (avaliableSections[id]) {
+      avaliableSections[id](id);
+    }
+  });
+});
+
+// render user profile by default
+//renderSection(createUserProfile());
